b4997c4aa127369a774bf05bef70c6b73b45d824
flebbas <71260510+flebbas@users.noreply.github.com>
2022-01-27 13:04:38 +0100

    2

---
+++ SnakeGame/Food.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    internal class Food : GameObject
+    {
+        public Food()
+        {
+
+        }
+        public override void Update()
+        {
+
+        }
+    }
+}
--- SnakeGame/GameObject.cs
+++ SnakeGame/GameObject.cs
@@ -7,7 +7,7 @@ namespace SnakeGame
     abstract class GameObject
     {
         public Position position = new Position();
-        public char Appearance = '@';
+        public char Appearance;
 
         public abstract void Update();
 
