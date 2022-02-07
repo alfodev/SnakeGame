fc59ec37d7fad460f62f3d1e5fc6d2dc8b1fb159
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2022-02-07 12:43:34 +0000

    Update Tail.cs

--- SnakeGame/Tail.cs
+++ SnakeGame/Tail.cs
@@ -7,34 +7,30 @@ using System.Threading.Tasks;
 namespace SnakeGame
 {
     /// <summary>
-    /// 
+    /// Information of objects position,appearance.
     /// </summary>
     public class Tail : GameObject
     {
-        //public char Appearance;
-        
         /// <summary>
-        /// 
+        /// Constructor that gives object its appearance and instance of Position
         /// </summary>
-        /// <param name="appearance"></param>
-        /// <param name="posX"></param>
-        /// <param name="posY"></param>
+        /// <param name="appearance">The char instance that represents objects appearance</param>
+        /// <param name="posX">The int instance that represents objects position horizontally</param>
+        /// <param name="posY">The int instance that represents objects position vertically</param>
         public Tail(char appearance,int posX,int posY) 
             : base(appearance,posX,posY)
         {
-            
-            //this.Appearance = appearance;
         }
 
         /// <summary>
-        /// 
+        /// Abstract method for object
         /// </summary>
         public override void Update()
         {
         }
 
         /// <summary>
-        /// 
+        /// Abstract method for object
         /// </summary>
         public override void AteFood()
         {
