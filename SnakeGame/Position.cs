af884dce9f85f58773d166bf611259cacaa03d6a
Misslittle87 <92373716+Misslittle87@users.noreply.github.com>
2022-02-07 14:22:35 +0100

    Kommentarer för player

--- SnakeGame/Position.cs
+++ SnakeGame/Position.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeGame
-    
+namespace SnakeGame    
 {
     /// <summary>
     /// A class which providing positioning of the different objects
@@ -14,21 +13,15 @@ namespace SnakeGame
     {
         public int X;
         public int Y;
-
-
         /// <summary>
         /// providing positioning of the diffent objects 
         /// </summary>
         /// <param name="startX">Represents an integer with the information about positioning horizontally</param>
         /// <param name="startY">Represents an integer with the information about positioning vertically</param>
-
-        // Denna konstruktor kanske skall använda sig av en random metod.
         public Position(int startX, int startY)
         {
            X = startX;
            Y = startY; 
-
         }
-
     }
 }
