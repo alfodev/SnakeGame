b108c71002736e964085cdbd9233fd15ec39d3b4
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2022-02-18 15:38:19 +0000

    Update SFMLRenderer.cs
    Mat, svans, huvud har olika former.
    En ram gjord.

--- SnakeGame/SFMLRenderer.cs
+++ SnakeGame/SFMLRenderer.cs
@@ -9,7 +9,6 @@ using SFML.System;
 
 namespace SnakeGame
 {
-
     class SFMLRenderer
     {
         static private int SIZE = 10; // Storlek på varje ruta
@@ -18,8 +17,7 @@ namespace SnakeGame
         public SFMLRenderer(GameWorld w)
         {
             world = w;
-            VideoMode windowSize = new VideoMode((uint)(w.Width *
-            SIZE), (uint)(w.Height * SIZE));
+            VideoMode windowSize = new VideoMode((uint)(w.Width * SIZE +10), (uint)(w.Height * SIZE +10));
             string title = "Snake 2.0";
             window = new RenderWindow(windowSize, title);
             window.SetActive();
@@ -27,37 +25,65 @@ namespace SnakeGame
         private void RenderBorder()
         {
             RectangleShape line = new RectangleShape();
-            line.Size = new Vector2f(world.Width * SIZE, 1 * SIZE);
-            line.Position = new Vector2f(0, 0);
             line.FillColor = Color.Yellow;
+            line.Position = new Vector2f(0, 0);
+            line.Size = new Vector2f(world.Width * SIZE, 1 * SIZE);            
             window.Draw(line);
 
-            line.Position = new Vector2f(0, world.Height * SIZE - SIZE);
+            line.Rotation = 0;
+            line.FillColor = Color.Blue;
+            line.Size = new Vector2f(1 * SIZE, world.Width * SIZE + 20);            
             window.Draw(line);
 
-            line.Size = new Vector2f(1 * SIZE, world.Width * SIZE);
+            line.FillColor = Color.Red;
+            line.Position = new Vector2f(world.Width * SIZE , 0 * SIZE);
+            window.Draw(line);
+
+            line.Rotation = 360;
+            line.FillColor = Color.Green;
+            line.Position = new Vector2f(0 * SIZE, world.Height * SIZE);
+            line.Size = new Vector2f(world.Width * SIZE, 1 * SIZE);  
             window.Draw(line);
         }
         public void Render()
-        {
-            
-
+        {   
             if (!window.IsOpen)
             {
                 return;
             }
+
             window.Clear(); // Motsvarigheten till Console.Clear()
             RenderBorder();
             window.DispatchEvents(); // Vi säger att vi tagit emot all info vi behöver
+            
+
             foreach (var obj in world.GameObj)
             {
-                CircleShape cirkel = new CircleShape(SIZE);
-                cirkel.FillColor = Color.Yellow;
-                cirkel.Position = new SFML.System.Vector2f(obj.pos.X * SIZE, obj.pos.Y * SIZE);
-                //Console.SetCursorPosition(obj.pos.X, obj.pos.Y);
-                window.Draw(cirkel);
-                //Console.Write(obj.appearance);
-            }                        // TODO rita ut allting i world
+                if (obj is Tail)
+                {
+                    CircleShape svans = new CircleShape(SIZE);
+                    svans.Position = new Vector2f(obj.pos.X * SIZE, obj.pos.Y * SIZE);
+                    svans.FillColor = Color.Cyan;
+                    window.Draw(svans);
+                }
+                if (obj is Player)
+                {
+                    CircleShape cirkel = new CircleShape(SIZE, 8);                    
+                    cirkel.FillColor = Color.Green;
+                    //cirkel.OutlineThickness = 2;
+                    //cirkel.OutlineColor = Color.Yellow;
+                    //cirkel.Radius = 6;
+                    cirkel.Position = new Vector2f(obj.pos.X * SIZE, obj.pos.Y * SIZE);
+                    window.Draw(cirkel);
+                }
+                if (obj is Food)
+                {
+                    CircleShape mat = new CircleShape(SIZE, 4);    
+                    mat.Position = new Vector2f(obj.pos.X * SIZE, obj.pos.Y * SIZE);
+                    mat.FillColor = Color.Red;
+                    window.Draw(mat);
+                }               
+            }      
             window.Display(); // Visa den nya framen i fönstret
         }
     }
